dfe4050148791ccc2b1a58a21389f7757f30b987
PeteRager <pete.sage@icloud.com>
2024-06-19 08:28:25 -0400

    initial refactor

--- homeassistant/components/sonos/media_player.py
+++ homeassistant/components/sonos/media_player.py
@@ -16,6 +16,7 @@ from soco.core import (
 )
 from soco.data_structures import DidlFavorite
 from soco.ms_data_structures import MusicServiceItem
+from soco.plugins.sharelink import ShareLinkPlugin
 from sonos_websocket.exception import SonosWebsocketError
 import voluptuous as vol
 
@@ -551,6 +552,115 @@ class SonosMediaPlayerEntity(SonosEntity, MediaPlayerEntity):
             partial(self._play_media, media_type, media_id, is_radio, **kwargs)
         )
 
+    def _play_media_plex(
+        self,
+        soco: SoCo,
+        media_type: MediaType | str,
+        media_id: str,
+        enqueue: MediaPlayerEnqueue,
+    ) -> None:
+        plex_plugin = self.speaker.plex_plugin
+        result = process_plex_payload(
+            self.hass, media_type, media_id, supports_playqueues=False
+        )
+        if result.shuffle:
+            self.set_shuffle(True)
+        if enqueue == MediaPlayerEnqueue.ADD:
+            plex_plugin.add_to_queue(result.media, timeout=LONG_SERVICE_TIMEOUT)
+        elif enqueue in (
+            MediaPlayerEnqueue.NEXT,
+            MediaPlayerEnqueue.PLAY,
+        ):
+            pos = (self.media.queue_position or 0) + 1
+            new_pos = plex_plugin.add_to_queue(
+                result.media, position=pos, timeout=LONG_SERVICE_TIMEOUT
+            )
+            if enqueue == MediaPlayerEnqueue.PLAY:
+                soco.play_from_queue(new_pos - 1)
+        elif enqueue == MediaPlayerEnqueue.REPLACE:
+            soco.clear_queue()
+            plex_plugin.add_to_queue(result.media, timeout=LONG_SERVICE_TIMEOUT)
+            soco.play_from_queue(0)
+
+    def _play_media_share_link(
+        self,
+        soco: SoCo,
+        share_link: ShareLinkPlugin,
+        media_id: str,
+        enqueue: MediaPlayerEnqueue,
+    ) -> None:
+        if enqueue == MediaPlayerEnqueue.ADD:
+            share_link.add_share_link_to_queue(media_id, timeout=LONG_SERVICE_TIMEOUT)
+        elif enqueue in (
+            MediaPlayerEnqueue.NEXT,
+            MediaPlayerEnqueue.PLAY,
+        ):
+            pos = (self.media.queue_position or 0) + 1
+            new_pos = share_link.add_share_link_to_queue(
+                media_id, position=pos, timeout=LONG_SERVICE_TIMEOUT
+            )
+            if enqueue == MediaPlayerEnqueue.PLAY:
+                soco.play_from_queue(new_pos - 1)
+        elif enqueue == MediaPlayerEnqueue.REPLACE:
+            soco.clear_queue()
+            share_link.add_share_link_to_queue(media_id, timeout=LONG_SERVICE_TIMEOUT)
+            soco.play_from_queue(0)
+
+    def _play_media_track(
+        self, soco: SoCo, media_id: str, enqueue: MediaPlayerEnqueue, is_radio: bool
+    ) -> None:
+        # If media ID is a relative URL, we serve it from HA.
+        media_id = async_process_play_media_url(self.hass, media_id)
+
+        if enqueue == MediaPlayerEnqueue.ADD:
+            soco.add_uri_to_queue(media_id, timeout=LONG_SERVICE_TIMEOUT)
+        elif enqueue in (
+            MediaPlayerEnqueue.NEXT,
+            MediaPlayerEnqueue.PLAY,
+        ):
+            pos = (self.media.queue_position or 0) + 1
+            new_pos = soco.add_uri_to_queue(
+                media_id, position=pos, timeout=LONG_SERVICE_TIMEOUT
+            )
+            if enqueue == MediaPlayerEnqueue.PLAY:
+                soco.play_from_queue(new_pos - 1)
+        elif enqueue == MediaPlayerEnqueue.REPLACE:
+            soco.play_uri(media_id, force_radio=is_radio)
+
+    def _play_media_playlist(
+        self, soco: SoCo, media_type: MediaType | str, media_id: str
+    ) -> None:
+        if media_id.startswith("S:"):
+            playlist = media_browser.get_media(self.media.library, media_id, media_type)
+        else:
+            playlists = soco.get_sonos_playlists(complete_result=True)
+            playlist = next((p for p in playlists if p.title == media_id), None)
+        if not playlist:
+            _LOGGER.error('Could not find a Sonos playlist named "%s"', media_id)
+        else:
+            soco.clear_queue()
+            soco.add_to_queue(playlist, timeout=LONG_SERVICE_TIMEOUT)
+            soco.play_from_queue(0)
+
+    def _play_media_playable_types(
+        self,
+        soco: SoCo,
+        media_type: MediaType | str,
+        media_id: str,
+        enqueue: MediaPlayerEnqueue,
+    ) -> None:
+        item = media_browser.get_media(self.media.library, media_id, media_type)
+        if not item:
+            _LOGGER.error('Could not find "%s" in the library', media_id)
+            return
+        self._play_media_queue(soco, item, enqueue)
+
+    def _play_media_favorite_item_id(self, soco: SoCo, media_id: str) -> None:
+        favorite = self.speaker.favorites.lookup_by_item_id(media_id)
+        if favorite is None:
+            raise ValueError(f"Missing favorite for media_id: {media_id}")
+        self._play_favorite(favorite)
+
     @soco_error()
     def _play_media(
         self, media_type: MediaType | str, media_id: str, is_radio: bool, **kwargs: Any
@@ -558,102 +668,21 @@ class SonosMediaPlayerEntity(SonosEntity, MediaPlayerEntity):
         """Wrap sync calls to async_play_media."""
         _LOGGER.debug("_play_media media_type %s media_id %s", media_type, media_id)
         enqueue = kwargs.get(ATTR_MEDIA_ENQUEUE, MediaPlayerEnqueue.REPLACE)
+        soco = self.coordinator.soco
+        share_link = self.coordinator.share_link
 
         if media_type == "favorite_item_id":
-            favorite = self.speaker.favorites.lookup_by_item_id(media_id)
-            if favorite is None:
-                raise ValueError(f"Missing favorite for media_id: {media_id}")
-            self._play_favorite(favorite)
-            return
-
-        soco = self.coordinator.soco
-        if media_id and media_id.startswith(PLEX_URI_SCHEME):
-            plex_plugin = self.speaker.plex_plugin
-            result = process_plex_payload(
-                self.hass, media_type, media_id, supports_playqueues=False
-            )
-            if result.shuffle:
-                self.set_shuffle(True)
-            if enqueue == MediaPlayerEnqueue.ADD:
-                plex_plugin.add_to_queue(result.media, timeout=LONG_SERVICE_TIMEOUT)
-            elif enqueue in (
-                MediaPlayerEnqueue.NEXT,
-                MediaPlayerEnqueue.PLAY,
-            ):
-                pos = (self.media.queue_position or 0) + 1
-                new_pos = plex_plugin.add_to_queue(
-                    result.media, position=pos, timeout=LONG_SERVICE_TIMEOUT
-                )
-                if enqueue == MediaPlayerEnqueue.PLAY:
-                    soco.play_from_queue(new_pos - 1)
-            elif enqueue == MediaPlayerEnqueue.REPLACE:
-                soco.clear_queue()
-                plex_plugin.add_to_queue(result.media, timeout=LONG_SERVICE_TIMEOUT)
-                soco.play_from_queue(0)
-            return
-
-        share_link = self.coordinator.share_link
-        if share_link.is_share_link(media_id):
-            if enqueue == MediaPlayerEnqueue.ADD:
-                share_link.add_share_link_to_queue(
-                    media_id, timeout=LONG_SERVICE_TIMEOUT
-                )
-            elif enqueue in (
-                MediaPlayerEnqueue.NEXT,
-                MediaPlayerEnqueue.PLAY,
-            ):
-                pos = (self.media.queue_position or 0) + 1
-                new_pos = share_link.add_share_link_to_queue(
-                    media_id, position=pos, timeout=LONG_SERVICE_TIMEOUT
-                )
-                if enqueue == MediaPlayerEnqueue.PLAY:
-                    soco.play_from_queue(new_pos - 1)
-            elif enqueue == MediaPlayerEnqueue.REPLACE:
-                soco.clear_queue()
-                share_link.add_share_link_to_queue(
-                    media_id, timeout=LONG_SERVICE_TIMEOUT
-                )
-                soco.play_from_queue(0)
+            self._play_media_favorite_item_id(soco, media_id)
+        elif media_id and media_id.startswith(PLEX_URI_SCHEME):
+            self._play_media_plex(soco, media_type, media_id, enqueue)
+        elif share_link.is_share_link(media_id):
+            self._play_media_share_link(soco, share_link, media_id, enqueue)
         elif media_type in {MediaType.MUSIC, MediaType.TRACK}:
-            # If media ID is a relative URL, we serve it from HA.
-            media_id = async_process_play_media_url(self.hass, media_id)
-
-            if enqueue == MediaPlayerEnqueue.ADD:
-                soco.add_uri_to_queue(media_id, timeout=LONG_SERVICE_TIMEOUT)
-            elif enqueue in (
-                MediaPlayerEnqueue.NEXT,
-                MediaPlayerEnqueue.PLAY,
-            ):
-                pos = (self.media.queue_position or 0) + 1
-                new_pos = soco.add_uri_to_queue(
-                    media_id, position=pos, timeout=LONG_SERVICE_TIMEOUT
-                )
-                if enqueue == MediaPlayerEnqueue.PLAY:
-                    soco.play_from_queue(new_pos - 1)
-            elif enqueue == MediaPlayerEnqueue.REPLACE:
-                soco.play_uri(media_id, force_radio=is_radio)
+            self._play_media_track(soco, media_id, enqueue, is_radio)
         elif media_type == MediaType.PLAYLIST:
-            if media_id.startswith("S:"):
-                playlist = media_browser.get_media(
-                    self.media.library, media_id, media_type
-                )
-            else:
-                playlists = soco.get_sonos_playlists(complete_result=True)
-                playlist = next((p for p in playlists if p.title == media_id), None)
-            if not playlist:
-                _LOGGER.error('Could not find a Sonos playlist named "%s"', media_id)
-            else:
-                soco.clear_queue()
-                soco.add_to_queue(playlist, timeout=LONG_SERVICE_TIMEOUT)
-                soco.play_from_queue(0)
+            self._play_media_playlist(soco, media_type, media_id)
         elif media_type in PLAYABLE_MEDIA_TYPES:
-            item = media_browser.get_media(self.media.library, media_id, media_type)
-
-            if not item:
-                _LOGGER.error('Could not find "%s" in the library', media_id)
-                return
-
-            self._play_media_queue(soco, item, enqueue)
+            self._play_media_playable_types(soco, media_type, media_id, enqueue)
         else:
             _LOGGER.error('Sonos does not support a media type of "%s"', media_type)
 
